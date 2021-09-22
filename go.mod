1acdf8e1b47f25c985802c9ab95531feb20f57d0
huan-Mongo <huan.li@mongodb.com>
2021-09-22 19:23:13 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module testmock.com/test
+
+go 1.16
+
+require (
+	github.com/pkg/errors v0.9.1 // indirect
+	go.mongodb.org/mongo-driver v1.7.2
+)
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/event"
+	"go.mongodb.org/mongo-driver/mongo"
+	mt "go.mongodb.org/mongo-driver/mongo/integration/mtest"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	_ "go.mongodb.org/mongo-driver/mongo/options"
+	"testmock.com/test/replitest"
+)
+
+var mockDeployment *replitest.MockDeployment
+
+func newTestClient() *mongo.Client {
+
+	clientOpts := options.Client().SetWriteConcern(mt.MajorityWc).SetReadPreference(mt.PrimaryRp)
+	var started []*event.CommandStartedEvent
+	var succeeded []*event.CommandSucceededEvent
+	var failed []*event.CommandFailedEvent
+
+	// command monitor
+	clientOpts.SetMonitor(&event.CommandMonitor{
+		Started: func(_ context.Context, cse *event.CommandStartedEvent) {
+			started = append(started, cse)
+		},
+		Succeeded: func(_ context.Context, cse *event.CommandSucceededEvent) {
+			succeeded = append(succeeded, cse)
+		},
+		Failed: func(_ context.Context, cfe *event.CommandFailedEvent) {
+			failed = append(failed, cfe)
+		},
+	})
+	connsCheckedOut := 0
+
+	// only specify connection pool monitor if no deployment is given
+	if clientOpts.Deployment == nil {
+		previousPoolMonitor := clientOpts.PoolMonitor
+
+		clientOpts.SetPoolMonitor(&event.PoolMonitor{
+			Event: func(evt *event.PoolEvent) {
+				if previousPoolMonitor != nil {
+					previousPoolMonitor.Event(evt)
+				}
+
+				switch evt.Type {
+				case event.GetSucceeded:
+					connsCheckedOut++
+				case event.ConnectionReturned:
+					connsCheckedOut--
+				}
+			},
+		})
+	}
+
+	clientOpts.PoolMonitor = nil
+	mockDeployment = replitest.NewMockDeployment()
+	clientOpts.Deployment = mockDeployment
+	client, _:= mongo.NewClient(clientOpts)
+	return client
+}
+
+func main()  {
+	client := newTestClient()
+	client.Connect(context.Background())
+
+	mockDeployment.AddResponses(mt.CreateSuccessResponse())
+
+	coll := client.Database("test").Collection("test")
+	ret, err := coll.InsertOne(context.Background(), bson.D{})
+	fmt.Println(ret)
+	fmt.Println(err)
+
+	ns := coll.Database().Name() + "." + coll.Name()
+	aggregateRes := mt.CreateCursorResponse(1, ns, mt.FirstBatch, bson.D{
+		{"_id", bson.D{{"first", "resume token"}}},
+	})
+
+	mockDeployment.AddResponses(aggregateRes)
+	cs, err := coll.Watch(context.Background(), bson.D{})
+	fmt.Println(err)
+	if err == nil  {
+		// if there was no error and an error is expected, capture the result from iterating the stream once
+		fmt.Println(cs.Next(context.Background()))
+		err = cs.Err()
+		fmt.Println(err)
+	}
+
+
+}
